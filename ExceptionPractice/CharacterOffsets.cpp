babb38c567fa57da755b8e5f1fb32f97ed2a6f06
beelover715 <charles.schneider@sinclair.edu>
2024-12-04 18:19:10 -0500

    Created first instance of character conversion function with exception throws.

--- ExceptionPractice/CharacterOffsets.cpp
+++ ExceptionPractice/CharacterOffsets.cpp
@@ -3,11 +3,29 @@
 // December 4th, 2024
 
 #include <iostream>
+#include <cstring>
 
 using namespace std;
+
+//function prototypes
+char character(char, int);
 
 int main() {
 
 
 	return 0;
+}
+
+char character(char start, int offset) {
+	if (isalpha(start) && isalpha(char(start + offset))) {
+		return (char(start + offset));
+	}
+	else if (!isalpha(start)) {
+		string invalidCharacterException = "Input was not a character.\n";
+		throw invalidCharacterException;
+	}
+	else if (!isalpha(char(start + offset))) {
+		string invalidRangeException = "Offset is too great";
+		throw invalidRangeException;
+	}
 }
